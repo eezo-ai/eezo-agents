a6dd5e6ac995ff28fc0a3f2fabf6f668db172191
Daniel S <daniel@tablestock.io>
2024-06-04 18:42:07 +0200

    Add text to chart tool

---
+++ demos/text_to_chart.py
@@ -0,0 +1,183 @@
+from langchain.pydantic_v1 import BaseModel
+from langchain.tools import BaseTool
+from typing import Type
+from eezo.interface.message import Message
+from eezo import Eezo
+from groq import Groq
+
+import dotenv
+
+dotenv.load_dotenv()
+
+AGENT_ID = "ea508f14-8aaf-4a14-a8f4-3efacf23832b"
+
+# Add EEZO_API_KEY to your .env file
+e = Eezo(logger=True)
+g = Groq()
+
+
+eezo_ui_api = """
+TEXT:
+Parameters:
+- text: str (The main text content for this component)
+
+Example:
+```python
+self.message.add("text", text="Hello World!")
+```
+
+CHART:
+Parameters:
+- chart_type: str (Available types: ["donut","pie","heatmap","radar","polarArea","radialBar","bar-horizontal","bar-stacked","bar","line-area","line","candlestick","treemap","scatter"])
+- name: str (Legend for the chart)
+- xaxis: List[str] (X-axis labels)
+- chart_title: str (Title of the chart)
+
+Depending on the chart type, the format of 'data' varies:
+
+# Data for donut, pie, treemap:
+```python
+data = [10, 20, 30]  # List of datapoints
+```
+
+# Data for candlestick:
+```python
+data = [
+    [open1, high1, low1, close1],
+    [open2, high2, low2, close2],
+    ...
+]  # List of [open, high, low, close]
+```
+
+# Data for scatter:
+```python
+data = [{
+    "data": [[x1, y1], [x2, y2], ...],
+    "name": "Legend for scatter data",
+}]
+```
+
+# Data for all others:
+```python
+data = [{
+    "data": [10, 20, 30],
+    "name": "Legend for data series",
+}]
+```
+
+Example usage:
+```python
+self.message.add('chart', chart_type="bar", data=[{"data": [10, 20, 30], "name": "Monthly Sales"}], name="Sales", xaxis=["Jan", "Feb", "Mar"], chart_title="Monthly Sales Overview")
+```
+
+IMAGE:
+Parameters:
+- url: str (The URL of the image)
+
+Example:
+```python
+self.message.add("image", url="https://example.com/image.jpg")
+```
+
+YOUTUBE VIDEO:
+Parameters:
+- video_id: str (The ID of the video)
+
+Example:
+```python
+self.message.add("youtube_video", video_id="xyz")
+```
+"""
+
+
+class GenerateUIArgsSchema(BaseModel):
+    query: str
+
+
+class GenerateUI(BaseTool):
+    name: str = "Generate UI"
+    description: str = "Generate UI components based on the given data and user prompt."
+    args_schema: Type[BaseModel] = GenerateUIArgsSchema
+    message: Message | None
+    thread: str | None
+
+    def __init__(
+        self, message: Message | None = None, thread: str = "No history privided."
+    ):
+        super().__init__()
+        self.message = message
+        self.thread = thread
+
+    def _run(self, **kwargs):
+        system_prompt = f"""
+You are a world class python coder, a expert from Stanford. You have been asked to summarize a section for a research to answer one specific question by generating UI components and their content. You have a set of Python UI components at your disposal:
+
+Components:
+{eezo_ui_api}
+
+The data you need to decide which UI components to generate and to fill with content:
+{self.thread}
+
+User prompt for the summary: 
+{self.message}
+
+Instructions:
+- Only use the components provided above.
+- You can use every component multiple times.
+- Use images when a url is provided.
+- Use youtube videos when a video_id is provided.
+- Use self.message to add new components. self.message is provided for you.
+- Last line of the script should be 'self.message.notify()' to display the UI.
+- Always include urls like [name](url) for references.
+- Don't add the code itself or other code explanations as a part of the UI.
+- Use markup to format the text component with bullet points and **bold** text.
+- Be concise and quote as much numbers and facts as possible. 
+- Don't just copy the data. Answer the research question.
+- Use charts only when you have to put multiple datapoints in relationship.
+- It is important to include sources and references after each sentence or paragraph using Markup, like [1](https://www.example.com) in the text and in the list at the end of the summary with: **1:** [link name](https://www.example.com).
+- Only check the last message in the chat history for context.
+
+Now, complete the code below to generate the UI:
+```python
+self.message.add("text", text="Answer:")
+
+# Add your UI components here
+"""
+
+        completion = g.chat.completions.create(
+            model="llama3-70b-8192",
+            temperature=0.7,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {
+                    "role": "user",
+                    "content": "Generate a UI to summarize the given data.",
+                },
+            ],
+        )
+        result = completion.choices[0].message.content
+        ui_str = result.split("```python\n")[1].split("\n```")[0]
+
+        try:
+            exec(ui_str)
+        except Exception as e:
+            self.message.add("text", text="An error occurred while executing the code:")
+            self.message.add("text", text=f"Error: {e}")
+            self.message.notify()
+
+
+@e.on(AGENT_ID)
+def text_to_chart(context, **kwargs):
+    m: Message = context.new_message()
+    m.add("text", text="Generate UI...")
+    m.notify()
+
+    thread = context.get_thread(to_string=True)
+
+    gen_ui = GenerateUI(m, thread)
+    gen_ui.invoke(input={"query": kwargs["query"]})
+
+    m.notify()
+
+
+e.connect()
